299fcd0162b4d7df27f99743391763ef74bd8ce7
Patrick Odeh <vyltyan@gmail.com>
2024-10-20 04:49:53 +0000

    0-uniq_users.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,11 +1,14 @@
 --  script that lists all bands with Glam rock
-SELECT 
-    band_name, 
-    COALESCE(split, 2022) - formed AS lifespan
-FROM 
-    metal_bands
-WHERE 
-    style = 'Glam rock'
-ORDER BY 
-    lifespan DESC;
-
+SELECT
+	band_name,
+	CASE
+		WHEN formed IS NOT NULL AND (split IS NULL OR split >= formed)
+		THEN COALESCE(split, 2022) - formed
+		ELSE NULL
+	END AS lifespan
+FROM
+	metal_bands
+WHERE
+	style = 'Glam rock'
+ORDER BY
+	lifespan DESC;
