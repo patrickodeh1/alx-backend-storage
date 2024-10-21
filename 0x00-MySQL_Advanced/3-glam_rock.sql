6031c4a8eda6ee19177e673a69297adeecd59bfc
Patrick Odeh <vyltyan@gmail.com>
2024-10-21 06:39:26 +0100

    4-store.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,13 @@
 -- SQL script to list all bands with Glam rock
-SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+SELECT 
+    name AS band_name, 
+    CASE 
+        WHEN split IS NOT NULL THEN split - formed  -- Band has split
+        ELSE 2020 - formed  -- Band is still active, using 2020 as the current year
+    END AS lifespan
+FROM 
+    metal_bands
+WHERE 
+    main_style LIKE '%Glam rock%'
+ORDER BY 
+    lifespan DESC;
